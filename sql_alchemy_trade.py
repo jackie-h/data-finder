2cc2eeb2b9ad37cdf15e7e360ec15478193c4aff
jackie-h <10471950+jackie-h@users.noreply.github.com>
2024-08-23 09:52:23 -0400

    Clean-up

--- sql_alchemy_trade.py
+++ sql_alchemy_trade.py
@@ -1,17 +1,11 @@
 import sqlalchemy as db
 
 
-
-
 def sql_alchemy_demo():
 
     engine = db.create_engine("sqlite:///test.sqlite")
 
     conn = engine.connect()
-    #conn = kx.QConnection('localhost', 5000)
-    #qvec = conn('2+til 2')
-    #kx.q.sql()
-    print(kx.q('1+1'))
 
 
 if __name__ == '__main__':
