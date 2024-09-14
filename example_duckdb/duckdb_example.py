85fb2ab01ce4d3c8b1e198f2a10e75bf953f5052
jackie-h <10471950+jackie-h@users.noreply.github.com>
2024-09-14 13:43:39 -0400

    Simple duckdb version working

--- example_duckdb/duckdb_example.py
+++ example_duckdb/duckdb_example.py
@@ -3,8 +3,13 @@ import duckdb
 from duckdb_trade_finder import *
 
 def duckdb_sample():
-    con = duckdb.connect()
+    con = duckdb.connect('test.db')
     con.sql("SELECT 42 AS x").show()
+    con.execute("DROP TABLE IF EXISTS trade;")
+    con.execute(
+        "CREATE TABLE trade(id INT, account_id INT, sym VARCHAR, price DOUBLE); COPY trade FROM '../data/trades.csv'")
+    con.sql("SELECT * from trade").show()
+    con.sql("SELECT * from trade where sym LIKE 'AAPL'").show()
 
 
 def find_trades():
