c6a633eacb428c133a50ae3c4f171a3cd0ca1c9a
jackie-h <10471950+jackie-h@users.noreply.github.com>
2025-01-04 14:29:21 -0500

    Add complex properties - Account

--- example/trade_mapping.py
+++ example/trade_mapping.py
@@ -1,15 +1,6 @@
 from m3 import Class, Property, String, Float, Package, Integer
 from relational import Column, Table, RelationalClassMapping, RelationalPropertyMapping
 
-
-def create_trade_class() -> Class:
-    p1 = Property('symbol', String)
-    p2 = Property('price', Float)
-
-    trade_c = Class('Trade', [p1, p2], Package('finance'))
-    return trade_c
-
-
 def create_account_class() -> Class:
     p1 = Property('id', Integer)
     p2 = Property('name', String)
@@ -18,8 +9,18 @@ def create_account_class() -> Class:
     return account_c
 
 
+def create_trade_class(account:Class) -> Class:
+    p1 = Property('symbol', String)
+    p2 = Property('price', Float)
+    p3 = Property('account', account)
+
+    trade_c = Class('Trade', [p1, p2, p3], Package('finance'))
+    return trade_c
+
+
 def create_mappings() -> list[RelationalClassMapping]:
-    trade_c = create_trade_class()
+    account_c = create_account_class()
+    trade_c = create_trade_class(account_c)
 
     c1 = Column('id', 'INT')
     c2 = Column('account_id', 'INT')
@@ -30,9 +31,9 @@ def create_mappings() -> list[RelationalClassMapping]:
 
     pm1 = RelationalPropertyMapping(trade_c.property('symbol'), c3)
     pm2 = RelationalPropertyMapping(trade_c.property('price'), c4)
-    rm_t = RelationalClassMapping(trade_c, [pm1, pm2])
+    pm3 = RelationalPropertyMapping(trade_c.property('account'),c2)
+    rm_t = RelationalClassMapping(trade_c, [pm1, pm2, pm3])
 
-    account_c = create_account_class()
     ac1 = Column('id', 'INT')
     ac2 = Column('name', 'VARCHAR')
     account_t = Table('account', [ac1, ac2])
--- example_ibis/ibis_gen.py
+++ example_ibis/ibis_gen.py
@@ -1,15 +1,21 @@
+from m3 import PrimitiveType, Property
 from trade_mapping import create_mappings
 from jinja2 import Environment, FileSystemLoader
+
+
+def is_primitive(prop: Property) -> bool:
+    return isinstance(prop.type, PrimitiveType)
+
 
 def generate():
     rcms = create_mappings()
 
-    environment = Environment(loader=FileSystemLoader("templates/"))
+    environment = Environment(loader=FileSystemLoader("templates/"),trim_blocks=True,lstrip_blocks=True)
     template = environment.get_template("finder_template.txt")
 
     for rcm in rcms:
         filename = f"{rcm.clazz.name.lower()}_finder.py"
-        content = template.render(rcm=rcm)
+        content = template.render(rcm=rcm,is_primitive=is_primitive)
         with open(filename, mode="w", encoding="utf-8") as message:
             message.write(content)
             print(f"... wrote {filename}")
