00a368cbdd64474af7c1bca173c253d38fb58ae2
jackie-h <10471950+jackie-h@users.noreply.github.com>
2025-09-03 21:40:22 -0400

    Add some basic date types as a placeholder

--- datafinder/src/datafinder/typed_attributes.py
+++ datafinder/src/datafinder/typed_attributes.py
@@ -41,3 +41,36 @@ class IntegerAttribute(Attribute):
 
     def __gt__(self, value: int) -> Operation:
         return PrimitiveGreaterThanOperation(self, value)
+
+
+class DateAttribute(Attribute):
+    """
+    Represents a date attribute without a time YYYY-MM-DD
+    """
+
+    def __init__(self, name: str, column_db_type: str, owner: str, parent=None):
+        super().__init__(name, column_db_type, owner, parent)
+
+    def eq(self, value: datetime.date) -> Operation:
+        return PrimitiveEqOperation(self, value)
+
+    def __eq__(self, value: datetime.date) -> Operation:
+        return PrimitiveEqOperation(self, value)
+
+    def __gt__(self, value: datetime.date) -> Operation:
+        return PrimitiveGreaterThanOperation(self, value)
+
+
+class DateTimeAttribute(Attribute):
+
+    def __init__(self, name: str, column_db_type: str, owner: str, parent=None):
+        super().__init__(name, column_db_type, owner, parent)
+
+    def eq(self, value: datetime.datetime) -> Operation:
+        return PrimitiveEqOperation(self, value)
+
+    def __eq__(self, value: datetime.datetime) -> Operation:
+        return PrimitiveEqOperation(self, value)
+
+    def __gt__(self, value: datetime.datetime) -> Operation:
+        return PrimitiveGreaterThanOperation(self, value)
